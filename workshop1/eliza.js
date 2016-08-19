0d0db7d9ca29152df6bae32b29bc3ef4d74bb86a
AveryG3 <avery.greenberg.333@gmail.com>
2016-08-19 12:24:32 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -28,9 +28,9 @@
  */
 
 var rules = {
-	"~*x avery ~*y" : [
-		"wow you are so great, avery, you are the best!"
-	],
+"~*x avery ~*y" : [
+  "wow you are so great, avery, you are the best!"
+],
 
   "~*x hello ~*y": [
     "How do you do. Please state your problem."
